f7e988cf8ad6a310e6832914c1b06a5421988f59
davidandstephens <stephens.a.david@gmail.com>
2018-11-20 09:49:21 -0600

    made the better fix

--- src/Author.java
+++ src/Author.java
@@ -3,13 +3,13 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-@SuppressWarnings("unchecked")
+
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -20,7 +20,6 @@ public class Author extends Person {
         return books;
     }
 
-    @SuppressWarnings("unused")
     public List<String> publishedBooks() {
         return books;
     }
